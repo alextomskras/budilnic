70b4b0bf704a8a93d04a86eb3acfa2f09efc0bb6
FESS <dreamer.alex.toms@gmail.com>
2019-08-21 22:24:17 +0300

    add-actionbutton-to-notification
    Signed-off-by: FESS <dreamer.alex.toms@gmail.com>

--- app/src/main/java/com/example/budilnic/Notifications.kt
+++ app/src/main/java/com/example/budilnic/Notifications.kt
@@ -1,12 +1,15 @@
 package com.example.budilnic
 
+import android.R
 import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Icon
+import android.media.RingtoneManager
 import android.os.Build
-import android.support.v4.app.NotificationCompat
+
 
 class Notifications {
 
@@ -14,17 +17,42 @@ class Notifications {
     fun Notify(context: Context,message:String,number:Int){
         val intent=Intent(context,MainActivity::class.java)
         val channelID = "com.example.budilnic.news"
+        val resultIntent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val icon: Icon = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Icon.createWithResource(context, android.R.drawable.ic_dialog_info)
+        } else {
+            return
+        }
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        val builder = NotificationCompat.Builder(context, channelID)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setContentTitle("New request")
+        val action: Notification.Action =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Notification.Action.Builder(icon, "Open", pendingIntent).build()
+            } else {
+                return
+            }
+
+        val builder = Notification.Builder(context, channelID)
+            .setDefaults(Notification.DEFAULT_VIBRATE)
+            .setContentTitle("New request222333")
                 .setContentText(message)
                 .setNumber(number)
             .setChannelId(channelID)
-                .setSmallIcon(R.drawable.notification_icon_background)
+            .setSound(defaultSoundUri)
+            .setSmallIcon(R.drawable.ic_lock_power_off)
+            .setSubText(message)
+            .addAction(action)
+//                .setSmallIcon(R.drawable.notification_icon_background)
                 .setContentIntent(PendingIntent.getActivity(context
                         ,0,intent,PendingIntent.FLAG_UPDATE_CURRENT))
-                .setAutoCancel(true)
+
+            .setAutoCancel(true)
 
         val nm=context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
